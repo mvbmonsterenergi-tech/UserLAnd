e45e510f79104f1bbc3bbdcc16bd481e8a7fcfed
Corbin Champion <corbinlc@gmail.com>
2025-09-12 17:09:03 -0700

    roll on pro feature request

--- app/src/main/assets/apps/debian/debian.sh
+++ app/src/main/assets/apps/debian/debian.sh
@@ -7,5 +7,10 @@ if [ -d /sdcard ]; then
     ln -s /sdcard ~/sdcard
   fi
 fi
+if [ -d /storage ]; then
+  if [ ! -d ~/scopedStorage ]; then
+    ln -s /storage/internal ~/scopedStorage
+  fi
+fi
 
 echo "Welcome to Debian in UserLAnd!"
